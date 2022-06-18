58a1522d4bcbc5432672f6b8594f4155c12c6afe
turleo <34786670+turleo@users.noreply.github.com>
2022-06-19 08:45:57 +1000

    произношение исправлено

--- app.py
+++ app.py
@@ -14,6 +14,15 @@ questions = ['Ты хочешь глубже узнать мир IT?',
              'У тебя есть опыт в веб-разработке?',
              'Умеешь выходить из VIM?']
 
+questions_tts = ['Ты хочешь ^глубже^ узнать мир ай ти?',
+                 '^Умеешь^ пользоваться консолью в`индоус?',
+                 'Знаешь хотя бы ^один^ язык программирования?',
+                 '^Разговаривал^ ли ты со змеёй?',
+                 '^Боишся^ ли ты порабощения человечества машинами?',
+                 'Сильно ли ты хорош в математике?',
+                 'У тебя есть опыт в веб разработке?',
+                 'Умеешь выходить из ^вим^?']
+
 changes = [
     {
         "y": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -62,12 +71,25 @@ answers = [
     'Маруся'
 ]
 
+answers_tts = [
+    'веб',
+    'компьютерное зрение',
+    'разработка игр',
+    'Мобильная разработка',
+    'Анализ данных',
+    'Дизайн интерфейсов',
+    'Оптимизация',
+    'Вк м`ини эпс',
+    'бэк энд',
+    'Маруся'
+]
+
 
 @app.route("/")
 def home():
     return 'по ссылке <pre>https://not-intellectual-cats-marusya.herokuapp.com/1</pre>' \
            ' первое задание, а <pre>https://not-intellectual-cats-marusya.herokuapp.com/2' \
-           '</pre> - второе<br/><sub>Мы просто не знали, что садть</sub>'
+           '</pre> - второе<br/><sub>Мы просто не знали, что садть<br/>а ещё там третье задание выполнено</sub>'
 
 
 @app.route("/1", methods=["POST"])
@@ -75,14 +97,18 @@ def first():
     data = request.get_json()
     if data["request"]["command"] in ["вездекод", "не интеллектуальные коты", "вездеход"]:
         text = "Привет вездекодерам!"
+        tts = "Привет ^вездек`одерам^"
     else:
         text = "Не привет"
+        tts = "Не привет"
     ans = {
         "response": {
             "text": text,
+            "tts": tts,
             "end_session": False
         },
-        "session": {derived_key: data["session"][derived_key] for derived_key in ['session_id', 'user_id', 'message_id']},
+        "session": {derived_key: data["session"][derived_key] for derived_key in
+                    ['session_id', 'user_id', 'message_id']},
         "version": data["version"]
     }
     return ans
@@ -97,13 +123,16 @@ def second():
 
     if data["session"]["message_id"] >= 8:
         answer = answers[session.index(max(session))]
+        tts = answers_tts[session.index(max(session))]
         return {
             "response": {
                 "text": answer + ' - то, что вам надо',
+                "tts": tts + ' - то, что ^вам^ надо',
                 "buttons": [],
                 "end_session": True
             },
-            "session": {derived_key: data["session"][derived_key] for derived_key in ['session_id', 'user_id', 'message_id']},
+            "session": {derived_key: data["session"][derived_key] for derived_key in
+                        ['session_id', 'user_id', 'message_id']},
             "version": data["version"]
         }
 
@@ -114,11 +143,12 @@ def second():
     sessions[data["session"]["session_id"]] = session
     print(session)
     text = questions[data["session"]["message_id"]]
-
+    tts = questions_tts[data["session"]["message_id"]]
 
     ans = {
         "response": {
             "text": text,
+            "tts": tts,
             "buttons": [
                 {
                     "title": "Да",
@@ -131,7 +161,8 @@ def second():
             ],
             "end_session": False
         },
-        "session": {derived_key: data["session"][derived_key] for derived_key in ['session_id', 'user_id', 'message_id']},
+        "session": {derived_key: data["session"][derived_key] for derived_key in
+                    ['session_id', 'user_id', 'message_id']},
         "version": data["version"]
     }
 
